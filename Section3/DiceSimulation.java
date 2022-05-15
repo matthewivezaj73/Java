d5312fd8623c796aecdc979ea743275915c14117
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-05-15 13:05:09 -0400

    Imported library.

--- Section3/DiceSimulation.java
+++ Section3/DiceSimulation.java
@@ -1,8 +1,10 @@
+//Importing library.
+import java.util.Random;
 //Creating a public class.
 public class DiceSimulation {
     //Creating a main method.
     public static void main(String[] args)
     {
-        
+
     }
 }
