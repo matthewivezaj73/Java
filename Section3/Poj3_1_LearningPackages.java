e6508433cb528be6e0578f3cf5aeab911ef4a948
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-05-15 16:30:35 -0400

    Added a comment.

--- Section3/Poj3_1_LearningPackages.java
+++ Section3/Poj3_1_LearningPackages.java
@@ -3,7 +3,7 @@ public class Poj3_1_LearningPackages {
     //Creating a main method.
     public static void main(String[] args)
     {
-    
+        //Adding a variable to accept user input.
     }
     
 }
