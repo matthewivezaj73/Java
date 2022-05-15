707fbf57428c53d48430bc87eee99d71cc214dce
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-05-15 13:59:46 -0400

    Removed for loop.

--- Section3/DiceSimulation.java
+++ Section3/DiceSimulation.java
@@ -11,14 +11,8 @@ public class DiceSimulation {
         int diceSimulator; 
         //Assigning a value to myRandomNumber.
         diceSimulator = dice.nextInt(6) + 1;
-        //Creating a for loop.
-        for(int i = 0; i < 11; i++)
-        {
-            //Printing the valuediceSimulator of the die.
-            System.out.println(i);
-            //Incrementing i.
-            i++;
-        }
+        System.out.println(dice);
+      
         
     }
 }
