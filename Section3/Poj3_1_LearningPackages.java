98c565b619e45e200b53fb26ab8617eb8da1390b
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-05-15 16:34:06 -0400

    Added a comment.

--- Section3/Poj3_1_LearningPackages.java
+++ Section3/Poj3_1_LearningPackages.java
@@ -4,6 +4,8 @@ public class Poj3_1_LearningPackages {
     //Creating a main method.
     public static void main(String[] args)
     {
+        //Creating a scanner object.
+        
         //Adding a variable to accept user input.
         String userInput;
         //Grabbing user input.
