737444153f153b00b43637253088196e6182d9ad
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-05-15 13:50:17 -0400

    Added a comment.

--- Section3/DiceSimulation.java
+++ Section3/DiceSimulation.java
@@ -16,6 +16,7 @@ public class DiceSimulation {
         {
             //Printing the valuediceSimulator of the die.
             System.out.println(i);
+            //Incrementing i.
         }
         
     }
