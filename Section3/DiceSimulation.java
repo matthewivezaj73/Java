9a12af25b063e1402635ee811bc9b33d6f11cc43
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-05-15 14:21:51 -0400

    Changed parameter.

--- Section3/DiceSimulation.java
+++ Section3/DiceSimulation.java
@@ -10,7 +10,7 @@ public class DiceSimulation {
         //Creating a variable.
         int diceSimulator; 
         //Assigning a value to myRandomNumber.
-        diceSimulator = dice.nextInt(1);
+        diceSimulator = dice.nextInt(7);
         //Creating a for loop.
         for(int i = 0; i < 7; i++)
         {
