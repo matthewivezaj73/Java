5c25088bcc12a1024ecde29e2af91d84b7a75099
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-05-15 14:46:09 -0400

    modified parameter.

--- Section3/DiceSimulation.java
+++ Section3/DiceSimulation.java
@@ -6,16 +6,16 @@ public class DiceSimulation {
     public static void main(String[] args)
     {
         //Creating a new dice object.
-        Random dice = new Random();
+        Random dice = new Random(6) ;
         //Creating a variable.
-        int diceSimulator; 
-        //Assigning a value to myRandomNumber.
-        diceSimulator = dice.nextInt(7);
+        // int diceSimulator; 
+        // //Assigning a value to myRandomNumber.
+        // diceSimulator = dice.nextInt(6)+ 1;
         //Creating a for loop.
-        for(int i = 0; i < 7; i++)
+        for(int i = 0; i < 11; i++)
         {
             //Printing the value of the dice.
-            System.out.println(diceSimulator+1);
+            System.out.println(dice);
             //Incrementing i.
             i++;
         }
