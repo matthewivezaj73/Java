ffaa4edcb6766296ba760aee3f1833b7efb86fa1
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-05-17 11:31:41 -0400

    Fixed the class

--- Section3/divisibleByThree.java
+++ Section3/divisibleByThree.java
@@ -6,7 +6,7 @@
 //Importing Scanner.
 import java.util.Scanner;
 //Creating a public class.
-public class DivisibleByThree {
+public class divisibleByThree {
     //Creating a mean method.
     public static void main(String[] args) 
     {
