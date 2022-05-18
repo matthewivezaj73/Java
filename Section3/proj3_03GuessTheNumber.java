45221f921c5b453e5c0cd90e1cce3a912b14869d
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-05-18 06:55:59 -0400

    Added a comment.

--- Section3/proj3_03GuessTheNumber.java
+++ Section3/proj3_03GuessTheNumber.java
@@ -43,6 +43,7 @@ public class proj3_03GuessTheNumber {
             //Telling the user that they guessed correctly!
             System.out.println("Congratulations, "+userInput+" was the correct number!");
         }
+        //Handling the case where the input does not match what is required.
         //Printing out the random number.
         System.out.println(myRandomNumber);
     }
