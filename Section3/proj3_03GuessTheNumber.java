06e9d318cac971537105c79f8d2c32b578a72e42
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-05-18 07:09:25 -0400

    Added an if statement.

--- Section3/proj3_03GuessTheNumber.java
+++ Section3/proj3_03GuessTheNumber.java
@@ -29,6 +29,10 @@ public class proj3_03GuessTheNumber {
         while(userInput != myRandomNumber)
         {
             //Handling the case where the number entered was not correct.
+            if(userInput != myRandomNumber)
+            {
+                
+            }
             //Asking the user to guess another number.
             System.out.println("Please guess new number:");
             //creating a variable to hold the users entry.
