31bbaae1d4fabc22662691c02a2cd93dcfb9e17a
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-05-18 06:52:02 -0400

    added a comment.

--- Section3/proj3_03GuessTheNumber.java
+++ Section3/proj3_03GuessTheNumber.java
@@ -31,9 +31,10 @@ public class proj3_03GuessTheNumber {
             //Notifing the user that the number generated is too low.
             System.out.println("Sorry, but, " + userInput + " is too low! Please try again.");
         }
+        //Handling the case where the userInput is too high.
         else if(userInput > myRandomNumber)
         {
-            
+
         }
         //Printing out the random number.
         System.out.println(myRandomNumber);
