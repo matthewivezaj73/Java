3d97abeedce14efc84abf158a04acfbb57fa186e
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-05-18 07:16:36 -0400

    Adding 1 onto the number of guesses.

--- Section3/proj3_03GuessTheNumber.java
+++ Section3/proj3_03GuessTheNumber.java
@@ -36,6 +36,7 @@ public class proj3_03GuessTheNumber {
                 //Notifing the user that the number generated is too low.
                 System.out.println("Sorry, but, \'" + userInput + "\'' is too low! Please try again.");
                 //Incrementing the number of guesses.
+                numberOfGuesses += 1;
             }
             //Handling the case where the userInput is too high.
             else if(userInput > myRandomNumber)
