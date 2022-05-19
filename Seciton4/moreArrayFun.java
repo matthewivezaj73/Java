eb0e183e6e2374ef59e48c0c98335644d0facf5e
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-05-19 07:19:36 -0400

    Removed randoms.

--- Seciton4/moreArrayFun.java
+++ Seciton4/moreArrayFun.java
@@ -14,8 +14,6 @@ public class moreArrayFun {
         //Creating a standard for loop.
         for(int num = 0; num < anArray.length; num++)
         {
-            //Creating a random variable.
-            myRandomNumber = random.nextInt(1,1000);
             //Assigning values to each element in the array.
             anArray[num] = myRandomNumber;
         }
