dcbde90d23b16f8fe0a3cdf6bd550134ebe4ed14
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-05-18 20:46:58 -0400

    Created a value at index 0.

--- Seciton4/moreArrayFun.java
+++ Seciton4/moreArrayFun.java
@@ -13,5 +13,6 @@ public class moreArrayFun {
         //Initialzing the array.
         anArray = new int[10];
         //Assigning values to each element in the array.
+        anArray[0] = 10
     }
 }
