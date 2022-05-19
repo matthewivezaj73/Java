1931f36882a23ad2523e94f2a3d8cc78fad13173
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-05-18 20:31:56 -0400

    Added an equal sign to the comparison.

--- Seciton4/ArrayFun.java
+++ Seciton4/ArrayFun.java
@@ -23,7 +23,7 @@ public class ArrayFun {
         myArray[4] = 20;
 
         //Creating a for loop.
-        for(int i = 0; i < myArray.length; i++)
+        for(int i = 0; i <= myArray.length; i++)
         {
             //Printing a message to the user.
             System.out.println(myArray[i]);
