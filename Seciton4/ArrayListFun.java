ff24f945ed55c9107556de66ecc071c22eec50b4
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-05-19 13:27:04 -0400

    Added a comment.

--- Seciton4/ArrayListFun.java
+++ Seciton4/ArrayListFun.java
@@ -1,8 +1,9 @@
+//Importing a library.
 //Creating a public class named ArrayListFun.
 public class ArrayListFun {
     //Creating a main method.
     public static void main(String[] args)
     {
-        
+
     }
 }
