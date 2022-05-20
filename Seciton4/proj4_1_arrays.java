6ea2eb5d2dc64e6e1d786814ee2258ce2a318c06
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-05-20 13:32:35 -0400

    Modified variable name.

--- Seciton4/proj4_1_arrays.java
+++ Seciton4/proj4_1_arrays.java
@@ -29,6 +29,6 @@ public class proj4_1_arrays
         //Asking the user for an integers.
         System.out.println("Please enter a number:\t");
         //Asking the user to enter a number.
-        userInput1 = keyboard.nextInt();
+        userInput = keyboard.nextInt();
     }
 }
