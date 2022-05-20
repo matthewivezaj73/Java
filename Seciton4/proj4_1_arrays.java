09198f1eba1ae7b166f761d8bc9cc8e7e0157267
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-05-20 06:56:33 -0400

    Created a public class.

--- Seciton4/proj4_1_arrays.java
+++ Seciton4/proj4_1_arrays.java
@@ -5,4 +5,11 @@
  */
 //Importing the arraylist package.
 import java.util.ArrayList;
-//Creating public class.
+public class proj4_1_arrays
+{
+    //Creating public class.
+    public static void main(String[] args)
+    {
+
+    }
+}
