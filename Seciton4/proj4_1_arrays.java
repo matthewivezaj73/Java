5cbfd34fe85c0885b962d1cf6baeab708d7fa8db
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-05-20 08:44:34 -0400

    Created another variable to store input.

--- Seciton4/proj4_1_arrays.java
+++ Seciton4/proj4_1_arrays.java
@@ -19,6 +19,7 @@ public class proj4_1_arrays
         ArrayList<Integer> myList = new ArrayList();
         //Created a variable to store user input.
         int userInput1;
+        int userInput2;
         //Asking the user for an integers.
         System.out.println("Please enter a number:\t");
         //Asking the user to enter a number.
