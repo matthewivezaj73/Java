6392ce13edbd3ec5753d6ca9ac030786d5c8f92d
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-05-20 07:21:50 -0400

    added a comment.

--- Seciton4/proj4_1_arrays.java
+++ Seciton4/proj4_1_arrays.java
@@ -17,5 +17,7 @@ public class proj4_1_arrays
         ArrayList<Integer> myList = new ArrayList();
         //Asking the user for 5 integers.
         System.out.println("Please enter a number:\t");
+        //Asking the user to enter a number.
+        
     }
 }
