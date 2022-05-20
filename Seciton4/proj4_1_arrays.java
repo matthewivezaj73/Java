955cf414a3d809784d9556562163527974a53ef5
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-05-20 07:24:40 -0400

    Modified comment.

--- Seciton4/proj4_1_arrays.java
+++ Seciton4/proj4_1_arrays.java
@@ -19,7 +19,7 @@ public class proj4_1_arrays
         ArrayList<Integer> myList = new ArrayList();
         //Created a variable to store user input.
         int userInput1;
-        //Asking the user for 5 integers.
+        //Asking the user for an integers.
         System.out.println("Please enter a number:\t");
         //Asking the user to enter a number.
         userInput1 = keyboard.nextInt();
