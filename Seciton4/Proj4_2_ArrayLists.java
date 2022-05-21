8012f65df58e6aebbcf5f3fe4670db3f07dbf763
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-05-21 07:54:39 -0400

    Added a print.

--- Seciton4/Proj4_2_ArrayLists.java
+++ Seciton4/Proj4_2_ArrayLists.java
@@ -10,6 +10,7 @@ public class Proj4_2_ArrayLists {
         //Creating a scanner object.
         Scanner keyboard = new Scanner(System.in);
         //Telling the user to enter a number greater that 0.
+        System.out.print("Please enter a positive number:\t");
         //Creating an arraylist.
         ArrayList<Integer> myList = new ArrayList();
     }
