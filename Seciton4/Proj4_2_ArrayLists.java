d4da8f0d6f83f6256f440a0dba7a106117fae26e
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-05-21 08:18:20 -0400

    Added a while loop.

--- Seciton4/Proj4_2_ArrayLists.java
+++ Seciton4/Proj4_2_ArrayLists.java
@@ -13,24 +13,27 @@ public class Proj4_2_ArrayLists {
         ArrayList<Integer> myList = new ArrayList();
         //Creating a variable.
         int userInput;
-        //Adding a while loop.
         //Telling the user to enter a number greater that 0.
         System.out.print("Please enter a positive number:\t");
         //Requesting user input.
         userInput = keyboard.nextInt();
-        //Creating an if else statement.
-        if(userInput > 0)
+        //Adding a while loop.
+        while(userInput < 0)
         {
-            //Adding the input to the array.
-            myList.add(userInput);
-            //Notifying the user that the input has been hadded to the array list.
-            System.out.println("Added!");
-        }
-        //Handling the case where the inputted value is greater than 0.
-        else
-        {
-            //Printing a message to the user.
-            System.out.println("Sorry, but, " + userInput + " is either a non numeric value or a negative value. Please try again!");
+            //Creating an if else statement.
+            if(userInput > 0)
+            {
+                //Adding the input to the array.
+                myList.add(userInput);
+                //Notifying the user that the input has been hadded to the array list.
+                System.out.println("Added!");
+            }
+            //Handling the case where the inputted value is greater than 0.
+            else
+            {
+                //Printing a message to the user.
+                System.out.println("Sorry, but, " + userInput + " is either a non numeric value or a negative value. Please try again!");
+            }
         }
     }
 }
