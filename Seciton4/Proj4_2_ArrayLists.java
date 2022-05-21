64f35700875bafb003141239e98cd3a24424b87b
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-05-21 17:58:05 -0400

    Changed param to 0.

--- Seciton4/Proj4_2_ArrayLists.java
+++ Seciton4/Proj4_2_ArrayLists.java
@@ -44,7 +44,7 @@ public class Proj4_2_ArrayLists {
             }
         }
         //Printing the list in reverse.
-        for(int myInt = myList.size() - 1; myInt >= 5; myInt--)
+        for(int myInt = myList.size() - 1; myInt >= 0; myInt--)
         {
             //Printing out each value.
             System.out.println(myList.get(myInt));
