e9db70abc53959b9924b2aed244c9f08eb293388
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-05-21 08:24:41 -0400

    modified comment.

--- Seciton4/Proj4_2_ArrayLists.java
+++ Seciton4/Proj4_2_ArrayLists.java
@@ -31,7 +31,7 @@ public class Proj4_2_ArrayLists {
                 //Incrementing countInteger.
                 countInteger += 1;
             }
-            //Handling the case where - is in the entry.
+            //Handling the case where userInput < 0 is in the entry.
             //Handling the case where the inputted value is greater than 0.
             else
             {
