2ec1f6e1bdb472971a9d843371057fb4925697e4
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-05-22 09:14:23 -0400

    Removed for loop.

--- Seciton4/Proj4_3_Parallel.java
+++ Seciton4/Proj4_3_Parallel.java
@@ -25,17 +25,12 @@ public class Proj4_3_Parallel {
         //Creating a while loop.
         while(placeCounter < 5)
         {
-            //Creating a for loop.
-            for(int i = 0; i < 1; i++)
-            {
                 //Asing for the individual's name.
                 System.out.print("What is the person's name:\t");
                 //Requesting user input.
                 individualName = keyboard.next();
                 //Adding the individual's name to the list.
                 IndividualNames.add(individualName);
-            }
-
             //Asking for the individual's age.
             System.out.print("What is the person's age:\t");
             //Requesting user input.
