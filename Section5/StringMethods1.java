0fe599c71002968911814907f3048be6aee345be
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-05-22 18:41:16 -0400

    Added a new string.

--- Section5/StringMethods1.java
+++ Section5/StringMethods1.java
@@ -6,5 +6,6 @@ public class StringMethods1 {
         //Creating variables.
         String name = "Matthew Ivezaj";
         String name2 = "Matthew Ivezaj";
+        String name3 = "John Doe";
     }
 }
