d99234d6dba863187491df2e5081e6728e410c98
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-05-22 09:05:46 -0400

    Added curly braces.

--- Seciton4/Proj4_3_Parallel.java
+++ Seciton4/Proj4_3_Parallel.java
@@ -24,36 +24,38 @@ public class Proj4_3_Parallel {
         System.out.print("Please enter 5 individual's names and ages.\n");
         //Creating a while loop.
         while(placeCounter < 5)
-        //Creating a for loop.
-        for(int i = 0; i < 1; i++)
         {
-            //Asing for the individual's name.
-            System.out.print("What is the person's name:\t");
-            //Requesting user input.
-            individualName = keyboard.nextLine();
+            //Creating a for loop.
+            for(int i = 0; i < 1; i++)
+            {
+                //Asing for the individual's name.
+                System.out.print("What is the person's name:\t");
+                //Requesting user input.
+                individualName = keyboard.nextLine();
 
-            //Checking to see if the name is 1 character or longer.
-            // if(individualName.length() > 0)
-            // {
-            //Printing a message to the user.
-            System.out.println("Adding the individual now!");
-            //Adding the individual's name to the list.
-            IndividualNames.add(individualName);
-            //Incrementing place counter.
-            placeCounter += 1;
-                
-            // }
+                //Checking to see if the name is 1 character or longer.
+                // if(individualName.length() > 0)
+                // {
+                //Printing a message to the user.
+                System.out.println("Adding the individual now!");
+                //Adding the individual's name to the list.
+                IndividualNames.add(individualName);
+                //Incrementing place counter.
+                placeCounter += 1;
+                    
+                // }
 
-        }
-        //Creating a for loop.
-        for(int myInt = 0; myInt < 1; myInt++)
-        {
-            //Asking for the individual's age.
-            System.out.print("What is the person's age:\t");
-            //Requesting user input.
-            individualAge = keyboard.nextInt();
-            //Adding the individual's age to the list.
-            IndividualAges.add(individualAge);
+            }
+            //Creating a for loop.
+            for(int myInt = 0; myInt < 1; myInt++)
+            {
+                //Asking for the individual's age.
+                System.out.print("What is the person's age:\t");
+                //Requesting user input.
+                individualAge = keyboard.nextInt();
+                //Adding the individual's age to the list.
+                IndividualAges.add(individualAge);
+            }
         }
     }
 }
