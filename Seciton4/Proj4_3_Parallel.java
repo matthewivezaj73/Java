daa3eaa90fe127fcfaff29befeb6c58d4030dfe1
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-05-21 21:21:50 -0400

    removed the break.

--- Seciton4/Proj4_3_Parallel.java
+++ Seciton4/Proj4_3_Parallel.java
@@ -33,8 +33,7 @@ public class Proj4_3_Parallel {
                 System.out.println("Adding the individual now!");
                 //Incrementing place counter.
                 placeCounter += 1;
-                //Breaking out of the loop.
-                break;
+
             }
             //Handling the alternative case.
             else
