dced5ea461e6b51c52dd17121559b7f5f7d21690
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-05-23 19:26:06 -0400

    extended string.

--- Section5/StringMethods2.java
+++ Section5/StringMethods2.java
@@ -4,6 +4,6 @@ public class StringMethods2 {
     public static void main(String[] args)
     {
         //Creating strings.
-        String myName = "John";
+        String myName = "John Baugh";
     }
 }
