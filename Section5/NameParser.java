bd4b9161fd0da5e72ba2eeef4d5c557795344228
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-05-23 20:54:43 -0400

    Pinting the position of the space.

--- Section5/NameParser.java
+++ Section5/NameParser.java
@@ -13,6 +13,7 @@ public class NameParser {
         String fullname = keyboard.nextLine();
         //Taking the index of the space.
         int space_in_name = fullname.indexOf(" "); 
+        System.out.println(space_in_name);
         //Taking the substring of the full name.
     }
 }
