18de997e8edb7fa10893d2e1bf9ed4256a71e9bb
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-05-23 21:04:22 -0400

    Printing the variable last.

--- Section5/NameParser.java
+++ Section5/NameParser.java
@@ -17,7 +17,7 @@ public class NameParser {
         String lastName = fullname.substring(space_in_name);
         //Creating an all lowercase version of the last name.
         String last = lastName.toLowerCase(); 
-        System.out.print(lastName);
+        System.out.print(last);
         // String lastNameIndex = fullname.substring();
         //Taking the substring of the full name.
     }
