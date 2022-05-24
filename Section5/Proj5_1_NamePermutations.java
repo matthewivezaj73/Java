b32b28caf5fec9939adcca6e7fad6c17412c565e
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-05-24 14:47:56 -0400

    added a comment.

--- Section5/Proj5_1_NamePermutations.java
+++ Section5/Proj5_1_NamePermutations.java
@@ -3,11 +3,12 @@
  *      05/24/2022
  *      Name Permutations Project
  */
+//Importing a library.
 //Creating a public class.
 public class Proj5_1_NamePermutations {
     //Creating the main method.
     public static void main(String[] args)
     {
-        
+
     }
 }
