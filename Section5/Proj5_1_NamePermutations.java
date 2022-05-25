08fbdae2b69392817bb45e23e95f0689e3c1c87f
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-05-24 20:21:17 -0400

    Changed the name of the arraylist.

--- Section5/Proj5_1_NamePermutations.java
+++ Section5/Proj5_1_NamePermutations.java
@@ -14,7 +14,7 @@ public class Proj5_1_NamePermutations {
         //Creating a scanner.
         Scanner keyboard = new Scanner(System.in);
         //Creating a list.
-        ArrayList<String> myList = new ArrayList<>();
+        ArrayList<String> firstNames = new ArrayList<>();
         //Creating a string to hold the name.
         String givenName;
         //Printing a message out to the user.
