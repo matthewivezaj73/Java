4ca78e0e1c020bc996e616d6c582c955ce9aaa20
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-05-24 20:18:11 -0400

    Added a comment.

--- Section5/Proj5_1_NamePermutations.java
+++ Section5/Proj5_1_NamePermutations.java
@@ -25,6 +25,7 @@ public class Proj5_1_NamePermutations {
         int findSpace = name.indexOf(" ");
         //Creating a new index to grab the last name.
         String last = name.substring(findSpace);
+        //Creating a new index for the first name.
         //Creating a new substring to grab the first name.
         String first = name.subString();
     }
