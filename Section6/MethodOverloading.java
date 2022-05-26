389e3f8186c9001563c74f8274057e0d2b96e7e2
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-05-25 20:31:04 -0400

    Overloaded result.

--- Section6/MethodOverloading.java
+++ Section6/MethodOverloading.java
@@ -12,6 +12,7 @@ public class MethodOverloading {
         //Printing the variable.
         System.out.println(result);
         //Overloading result.
+        result = getResult(5, 6);
     }
     //Creating a new method.
     public static int getResult(int num)
