891b9d44c23f8d89cfbfe0c746db8f988135b6ca
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-05-26 13:58:57 -0400

    Added a comment.

--- Section6/FunWith2DArrays.java
+++ Section6/FunWith2DArrays.java
@@ -54,6 +54,6 @@ public class FunWith2DArrays {
     //Creating another 2Darray method.
     public static void take2DArray(int[][] twoDArr)
     {
-        
+        //Creating an enhanced for loop.
     }
 }
