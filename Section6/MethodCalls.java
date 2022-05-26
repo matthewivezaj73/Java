70f2d099db53c4c6ab6108b70b82c186d700c88c
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-05-26 06:50:37 -0400

    Called the doSomething method.

--- Section6/MethodCalls.java
+++ Section6/MethodCalls.java
@@ -9,6 +9,7 @@ public class MethodCalls {
     public static void main(String[] args)
     {
         //Calling doSomething.
+        doSomething();
     }
 
     //Creating a method called doSomething.
