df13399c7cc6c593786f19fc52d5b6b92c1ffe0e
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-05-27 17:21:07 -0400

    Created  embedded for loop to access each element.

--- Section6/SumOfElements.java
+++ Section6/SumOfElements.java
@@ -29,6 +29,10 @@ public class SumOfElements {
         for(int j = 0; j < my2DArray.length; j++)
         {
             //Creating an embedded for loop to access each element.
+            for(int i = 0; i < my2DArray[j]; i++)
+            {
+                
+            }
         }
     }
 }
