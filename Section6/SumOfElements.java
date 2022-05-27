6d0ccf560c11c39ec2eef5c92835d3b68ccaf1d4
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-05-27 06:51:57 -0400

    added a comment.

--- Section6/SumOfElements.java
+++ Section6/SumOfElements.java
@@ -13,6 +13,8 @@ public class SumOfElements {
     {
         //Creating an arraylist.
         ArrayList<Integer> myArrayList = new ArrayList<>();
+        //Creating a variable and assigning it the newly created method.
+        
         //Creating a for loop to insert random numbers in the list.
     }   
     //Creating a public method.
