5cefad5f00a43a0c25d940a10d5cb2cbcf4af789
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-05-28 12:24:34 -0400

    Added a print statement.

--- Section6/Proj6_2_TicTacToe.java
+++ Section6/Proj6_2_TicTacToe.java
@@ -13,6 +13,7 @@ public class Proj6_2_TicTacToe {
         //Creating a scanner.
         Scanner keyboard = new Scanner(System.in);
         //Printing out a message to the user telling them to choose whether they want X or O.
+        System.out.println("Would you like to be X or O:\t");
         //Creating a variable to hold user input.
         int userInput1;
         //Accepting user input.
