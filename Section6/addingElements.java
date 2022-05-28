b85cd64d52f551bae1a28e6041898c1f6eb85a4c
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-05-28 11:38:01 -0400

    Passed an array list as arguments.

--- Section6/addingElements.java
+++ Section6/addingElements.java
@@ -20,8 +20,8 @@ public class addingElements {
         myArrayList.add(5324);
     }
     //Creating a new method to add the elements together.
-    public static void addElements()
+    public static void addElements(int[][] ArrayList)
     {
-        
+
     }
 }
