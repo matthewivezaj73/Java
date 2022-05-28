0a656ab2472d7a551fe61610e4ca920dbc39b027
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-05-28 11:35:38 -0400

    Added a number to the arraylist.

--- Section6/addingElements.java
+++ Section6/addingElements.java
@@ -13,5 +13,6 @@ public class addingElements {
         //Creating an arraylist.
         ArrayList<Integer> myArrayList = new ArrayList();
         //Adding a number to the arraylist.
+        myArrayList.add(4);
     }
 }
