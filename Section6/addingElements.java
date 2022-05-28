468316334f47656c58b3c1ca38997df4c256d53d
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-05-28 11:33:52 -0400

    Added a comment.

--- Section6/addingElements.java
+++ Section6/addingElements.java
@@ -3,11 +3,12 @@
     5/28/2022
     addingElements
 */
+//Importing arraylist.
 //Creating a public class.
 public class addingElements {
     //Creating a method.
     public static void main(String[] args)
     {
-        
+
     }
 }
