0d5e2fa64e7496f6fa12899ec24c34934257a265
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-05-28 12:38:20 -0400

    Incrementing myVariable.

--- Section6/Proj6_2_TicTacToe.java
+++ Section6/Proj6_2_TicTacToe.java
@@ -27,6 +27,7 @@ public class Proj6_2_TicTacToe {
                 //Printing a message to the user.
                 System.out.println("You are going first!");
                 //Incrementing the variable.
+                myVariable += 1;
             }
             //Handling the case where the user selects O.
             else if(userInput1 == "O")
