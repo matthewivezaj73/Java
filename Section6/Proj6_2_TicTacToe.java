9ca4ea9fde2693d34f41960613534da489b723ec
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-05-28 18:05:19 -0400

    Removed more code.

--- Section6/Proj6_2_TicTacToe.java
+++ Section6/Proj6_2_TicTacToe.java
@@ -10,8 +10,6 @@ public class Proj6_2_TicTacToe {
     //Creating a main method.
     public static void main(String[] args)
     {
-        //Creating a scanner.
-        Scanner keyboard = new Scanner(System.in);
         //Printing out a message to the user telling them to choose whether they want X or O.
         System.out.print("Would you like to be X or O:\t");
 
