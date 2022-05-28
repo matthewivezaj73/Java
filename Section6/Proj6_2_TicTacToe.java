641ac563b19a43b64f267a31774f299148bb0556
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-05-28 12:20:49 -0400

    Added a comment.

--- Section6/Proj6_2_TicTacToe.java
+++ Section6/Proj6_2_TicTacToe.java
@@ -8,7 +8,6 @@ public class Proj6_2_TicTacToe {
     //Creating a main method.
     public static void main(String[] args)
     {
-        //Printing a tictactoe board.
-        System.out.println("   |   |   ");
+        //Accepting user input.
     }
 }
