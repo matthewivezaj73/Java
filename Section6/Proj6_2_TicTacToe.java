a3bbb8cae9ba1ad96544be0c8c601d0e41514b28
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-05-28 18:16:41 -0400

    changed the comment.

--- Section6/Proj6_2_TicTacToe.java
+++ Section6/Proj6_2_TicTacToe.java
@@ -21,7 +21,7 @@ public class Proj6_2_TicTacToe {
         System.out.println("   |   |   ");
         //Printing out the next piece of the game board.
         System.out.println("___________");
-        //Printing out the next pidcd of the game board.
+        //Printing out the next piece of the game board.
         System.out.println("   |   |   ");
     }
     //Creating a method to start the game.
