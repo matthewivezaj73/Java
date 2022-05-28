dae9368744d47828c91afb9ce6b9ff0e3107b67d
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-05-28 17:36:16 -0400

    Cut code.

--- Section6/Proj6_2_TicTacToe.java
+++ Section6/Proj6_2_TicTacToe.java
@@ -14,35 +14,6 @@ public class Proj6_2_TicTacToe {
         Scanner keyboard = new Scanner(System.in);
         //Printing out a message to the user telling them to choose whether they want X or O.
         System.out.print("Would you like to be X or O:\t");
-        //Adding a variable.
-        int myVariable = 0;
-        //Creating a while loop.
-        while(myVariable != 1)
-        {
-            //Creating a variable to hold user input.
-            char userInput1 = keyboard.next().charAt(0);
-            //Checking if the user entered an X or an O.
-            if(userInput1 == 'X')
-            {
-                //Printing a message to the user.
-                System.out.println("You are going first!");
-                //Incrementing the variable.
-                myVariable += 1;
-            }
-            //Handling the case where the user selects O.
-            else if(userInput1 == 'O')
-            {
-                //Printing a message to the user.
-                System.out.println("You are going second!");
-                //Incrementing the variable.
-                myVariable += 2;
-            }
-            //Handling the case where the user enters something else.
-            else
-            {
-                //Printing out a message to the user instructing them to try entering an X or an O.
-                System.out.print("Please enter an X or an O:\t");
-            }
         }
     }
 }
