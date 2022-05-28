2bcd318c8e022fb30f21007077b3efca47287d8d
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-05-28 12:38:41 -0400

    Added a comment.

--- Section6/Proj6_2_TicTacToe.java
+++ Section6/Proj6_2_TicTacToe.java
@@ -34,6 +34,7 @@ public class Proj6_2_TicTacToe {
             {
                 //Printing a message to the user.
                 System.out.println("You are going second!");
+                //Incrementing the variable.
             }
             //Handling the case where the user enters something else.
             else
