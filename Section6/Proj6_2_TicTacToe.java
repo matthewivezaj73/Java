acf7c9214e5e6590caee2a956b4de6a22495edd2
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-05-29 10:56:57 -0400

    Added a comment.

--- Section6/Proj6_2_TicTacToe.java
+++ Section6/Proj6_2_TicTacToe.java
@@ -23,7 +23,7 @@ public class Proj6_2_TicTacToe {
     //Creating a method to print the game board.
     public static void printGameBoard(String[][] gameBoard)
     {
-
+        //Creating a keyboard scanner to allow for input.
     }
     //Creating a method to create game board.
     public static void setUpBoard(String[][] gameBoard)
