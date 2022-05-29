22026b291fa8325549f9942446acb83c8ef12959
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-05-29 10:18:23 -0400

    Passed an array.

--- Section6/Proj6_2_TicTacToe.java
+++ Section6/Proj6_2_TicTacToe.java
@@ -19,7 +19,7 @@ public class Proj6_2_TicTacToe {
 
     }
     //Creating a method to create game board.
-    public static void setUpBoard()
+    public static void setUpBoard(int[][] gameBoard)
     {
         //Inserting a blank line.
         System.out.println();
