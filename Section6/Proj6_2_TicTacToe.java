2fd369d51c23b7804e6cb8303091246b28a9be01
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-05-29 10:30:01 -0400

    Assigned a cell |.

--- Section6/Proj6_2_TicTacToe.java
+++ Section6/Proj6_2_TicTacToe.java
@@ -17,6 +17,7 @@ public class Proj6_2_TicTacToe {
         gameBoard[4][1] = "|";
         gameBoard[2][2] = "|";
         gameBoard[4][2] = "|";
+        gameBoard[2][3] = "|";
         //Calling the setUpBoard method to set up the tic tac toe board.
         setUpBoard(gameBoard);
         //Printing out a message to the user telling them to choose whether they want X or O.
