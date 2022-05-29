63f842781d7fa317c7bb9258c51dc1e0a967f1a8
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-05-29 19:37:14 -0400

    Added underscore.

--- Section6/Proj6_2_TicTacToe.java
+++ Section6/Proj6_2_TicTacToe.java
@@ -45,6 +45,7 @@ public class Proj6_2_TicTacToe {
         gameBoard[4][1] = "|";
         gameBoard[2][3] = "|";
         //Adding underscores.
+        gameBoard[3][0] = "_";
         gameBoard[3][1] = "_";
         gameBoard[3][2] = "_";
         gameBoard[3][3] = "_";
