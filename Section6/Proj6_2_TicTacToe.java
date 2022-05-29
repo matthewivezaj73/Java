18351bdb58c600b6fd5f74e4b72b9adddcb38bce
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-05-29 19:50:22 -0400

    Added a holder.

--- Section6/Proj6_2_TicTacToe.java
+++ Section6/Proj6_2_TicTacToe.java
@@ -50,6 +50,7 @@ public class Proj6_2_TicTacToe {
         //Assigning certain cells |.
         //Printing out the first vertical line.
         gameBoard[0][1] = "|";
+        gameBoard[1][1] = "*";
         gameBoard[2][1] = "|";
         gameBoard[4][1] = "|";
         //Printing out the second vertical line.
