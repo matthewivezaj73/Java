fb5aa5840bdbf8176c502bb1ea0550583e04883a
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-05-29 19:12:36 -0400

    Changed variable name to myString.

--- Section6/Proj6_2_TicTacToe.java
+++ Section6/Proj6_2_TicTacToe.java
@@ -27,7 +27,7 @@ public class Proj6_2_TicTacToe {
         for(String[] arr : Board)
         {
             //Creating a for loop to access the numbers.
-            for(int num : Board)
+            for(int myString : Board)
             {
                 //Printing out the numbers to the user.
                 System.out.print(num+ " ");
