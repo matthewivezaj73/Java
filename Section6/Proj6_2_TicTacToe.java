bd936daf90b766bc210626ac9d20b77e945c1eeb
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-05-29 12:14:13 -0400

    Placed method before initialization of gameboard.

--- Section6/Proj6_2_TicTacToe.java
+++ Section6/Proj6_2_TicTacToe.java
@@ -14,6 +14,8 @@ public class Proj6_2_TicTacToe {
         String[][] gameBoard = new String[5][5];
         //Calling the setUpBoard method to set up the tic tac toe board.
         // setUpBoard(gameBoard);
+        //Calling the startGame method to initialize the game.
+        startGame();
         //Calling printGameBoard.
         printGameBoard(gameBoard);
 
