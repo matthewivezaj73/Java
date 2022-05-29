3e664edbc0af3b5a44fb05cbc8e6fa827b40329a
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-05-29 10:31:28 -0400

    Changed parameter.

--- Section6/Proj6_2_TicTacToe.java
+++ Section6/Proj6_2_TicTacToe.java
@@ -18,7 +18,7 @@ public class Proj6_2_TicTacToe {
         gameBoard[2][3] = "|";
         gameBoard[4][3] = "|";
         gameBoard[2][5] = "|";
-        gameBoard[4][3] = "|";
+        gameBoard[4][5] = "|";
         //Calling the setUpBoard method to set up the tic tac toe board.
         setUpBoard(gameBoard);
         //Printing out a message to the user telling them to choose whether they want X or O.
