ea9c2a772d11772d5807f3e143ec3b1a64dc4f73
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-05-29 10:56:00 -0400

    Setup skeleton.

--- Section6/Proj6_2_TicTacToe.java
+++ Section6/Proj6_2_TicTacToe.java
@@ -21,6 +21,10 @@ public class Proj6_2_TicTacToe {
 
     }
     //Creating a method to print the game board.
+    public static void printGameBoard(int[][] gameBoard)
+    {
+        
+    }
     //Creating a method to create game board.
     public static void setUpBoard(String[][] gameBoard)
     {
