a0d91b2eb5ebaceb5d837412e31f6b591761e92e
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-05-29 14:16:18 -0400

    added a comment.

--- Section6/Proj6_2_TicTacToe.java
+++ Section6/Proj6_2_TicTacToe.java
@@ -53,6 +53,7 @@ public class Proj6_2_TicTacToe {
                 //Breaking out of the loop.
                 break;
             }
+            //Breaking from the loop.
         }
     }
     //Creating a new method.
