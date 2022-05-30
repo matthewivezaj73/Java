1880a19d330508368586ed042cca1b9f0f24593b
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-05-30 18:57:49 -0400

    added a comment.

--- Section6/Proj6_2_TicTacToe.java
+++ Section6/Proj6_2_TicTacToe.java
@@ -14,6 +14,7 @@ public class Proj6_2_TicTacToe {
         String[][] gameBoard = new String[5][5];
         //Adding a variable.
         String placeCounter;
+        //Adding a new scanner object.
         //Calling the startGame method to initialize the game.
         startGame();
         //Calling the setup board method to set up the board.
