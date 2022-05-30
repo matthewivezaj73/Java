830ad3d54df9ae824a2cfdc5237e631a7f1a73cf
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-05-30 18:56:10 -0400

    added a comment.

--- Section6/Proj6_2_TicTacToe.java
+++ Section6/Proj6_2_TicTacToe.java
@@ -20,6 +20,7 @@ public class Proj6_2_TicTacToe {
         setUpBoard(gameBoard);
         //Calling the print gameBoard method to print the game board.
         printGameBoard(gameBoard);
+        //Setting placeCounter equal to yes.
         //Creating a while loop to keep calling the following methods.
         while(placeCounter == "yes")
         {
