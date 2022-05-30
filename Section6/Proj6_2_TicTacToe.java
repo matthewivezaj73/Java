2315811452548cb74146cd0adfa67c0b7aab20e1
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-05-30 12:59:46 -0400

    Added a new line character.

--- Section6/Proj6_2_TicTacToe.java
+++ Section6/Proj6_2_TicTacToe.java
@@ -30,7 +30,7 @@ public class Proj6_2_TicTacToe {
         Scanner keyboard = new Scanner(System.in);
         //Adding some padding.
         System.out.println("\n");
-        System.out.println();
+        System.out.println("\n");
         System.out.println();
         //Asking the user to add a column they would like to enter their counter in.
         System.out.println("Please enter a 0 to place your counter in the first column.\n"+
