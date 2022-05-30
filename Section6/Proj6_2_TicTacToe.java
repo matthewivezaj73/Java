144124742800819933b301abf8a823a17a7c32e3
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-05-29 20:06:10 -0400

    Changed dialogue.

--- Section6/Proj6_2_TicTacToe.java
+++ Section6/Proj6_2_TicTacToe.java
@@ -41,7 +41,7 @@ public class Proj6_2_TicTacToe {
         else if(userInput == 2)
         {
             //Printing out a message to the user.
-            System.out.println("Good move!");
+            System.out.println("Good choice!");
         }
         //Handling the case where the user enters a 4.
         else if(userInput == 4)
