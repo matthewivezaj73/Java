63e34b21a2c0051656df51a1c215cdfc346abd3a
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-05-29 20:14:54 -0400

    Moved padding.

--- Section6/Proj6_2_TicTacToe.java
+++ Section6/Proj6_2_TicTacToe.java
@@ -28,6 +28,10 @@ public class Proj6_2_TicTacToe {
         int userInput;
         //Adding a scanner object.
         Scanner keyboard = new Scanner(System.in);
+        //Adding some padding.
+        System.out.println("\n");
+        System.out.println();
+        System.out.println();
         //Asking the user to add a column they would like to enter their counter in.
         System.out.println("Please enter a digit from 0 to place your counter in the first column.\n"+
         "Please enter a digit from 2 to place your counter in the second column\nPlease enter a digit from 4 to place your counter in the 3rd column");
