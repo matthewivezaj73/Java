32d2aefdbbdda94772e493bf3683085f701a5c21
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-05-30 13:05:19 -0400

    Created a new variable.

--- Section6/Proj6_2_TicTacToe.java
+++ Section6/Proj6_2_TicTacToe.java
@@ -27,6 +27,7 @@ public class Proj6_2_TicTacToe {
         //Creating a variable.
         int columnInput;
         int rowInput;
+        char charInput;
         //Adding a scanner object.
         Scanner keyboard = new Scanner(System.in);
         //Adding some padding.
