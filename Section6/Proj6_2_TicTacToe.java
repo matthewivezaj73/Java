3fb47f523b2f8b1ac25bd876a72acbb635c8ca83
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-05-30 18:53:25 -0400

    Removed unneeded code.

--- Section6/Proj6_2_TicTacToe.java
+++ Section6/Proj6_2_TicTacToe.java
@@ -36,9 +36,6 @@ public class Proj6_2_TicTacToe {
         System.out.println("\n");
         System.out.println("\n");
         System.out.println("\n");
-        //Setting no for the value of myCounter.
-        myCounter = "yes";
-
         //Asking the user to add a column they would like to enter their counter in.
         System.out.println("Please enter a 0 to place your counter in the first column.\n"+
         "Please enter a 2 to place your counter in the second column\nPlease enter a 4 to place your counter in the 3rd column");
