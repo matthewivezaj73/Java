66248db8f3523b2a9c399b8ad40e588254375c0f
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-05-30 18:28:05 -0400

    Changed string to char.

--- Section6/Proj6_2_TicTacToe.java
+++ Section6/Proj6_2_TicTacToe.java
@@ -29,7 +29,7 @@ public class Proj6_2_TicTacToe {
         //Creating a variable.
         int columnInput;
         int rowInput;
-        String charInput;
+        char charInput;
         int coordinates;
         //Adding a scanner object.
         Scanner keyboard = new Scanner(System.in);
