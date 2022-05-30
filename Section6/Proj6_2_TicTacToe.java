2e5a91751aa6f012c1461bd9a425f419cdf206c8
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-05-30 18:38:47 -0400

    added a comment.

--- Section6/Proj6_2_TicTacToe.java
+++ Section6/Proj6_2_TicTacToe.java
@@ -38,6 +38,7 @@ public class Proj6_2_TicTacToe {
         System.out.println("\n");
         System.out.println("\n");
         System.out.println("\n");
+        //Setting no for the value of myCounter.
         //Creating a while loop.
         while(myCounter != "yes")
         //Asking the user to add a column they would like to enter their counter in.
