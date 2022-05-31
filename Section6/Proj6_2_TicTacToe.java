5f90f27d5bd5034a9d0a6c2f2e2ac0931a9de8eb
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-05-31 14:47:59 -0400

    Modified print statement.

--- Section6/Proj6_2_TicTacToe.java
+++ Section6/Proj6_2_TicTacToe.java
@@ -29,7 +29,7 @@ public class Proj6_2_TicTacToe {
         //Calling the print gameBoard method to print the game board in it's current state.
         printGameBoard(gameBoard);
         //Printing a statement to the user.
-        System.out.println("Player with the O counter, it is your turn.");
+        System.out.println("Player 2, it is your turn.");
         //Calling the makeAMove method, so that the user will make a choice.
         makeAMovePlayerOne(gameBoard);
         //Calling the print gameBoard method to print the game board in it's current state.
