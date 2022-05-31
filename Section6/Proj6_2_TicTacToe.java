b04e8470fadae69a0edd0f444cc42f88fe260b7e
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-05-31 15:49:16 -0400

    added a comment.

--- Section6/Proj6_2_TicTacToe.java
+++ Section6/Proj6_2_TicTacToe.java
@@ -36,6 +36,7 @@ public class Proj6_2_TicTacToe {
         printGameBoard(gameBoard);
         //Calling the makeAmove method, so that the user will make a choice.
         makeAMovePlayerOne(gameBoard);
+        //Calling the printGameBoard method.
     }
     //Creating a method to allow the players to make their move.
     public static void makeAMovePlayerOne(String[][] gameBoard)
