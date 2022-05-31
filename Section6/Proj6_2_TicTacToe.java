f621fd23af0deb19ac2e699b5b3c77f1b58a3470
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-05-31 14:45:06 -0400

    Added a function call.

--- Section6/Proj6_2_TicTacToe.java
+++ Section6/Proj6_2_TicTacToe.java
@@ -33,6 +33,7 @@ public class Proj6_2_TicTacToe {
         //Calling the makeAMove method, so that the user will make a choice.
         makeAMove(gameBoard);
         //Calling the print gameBoard method to print the game board in it's current state.
+        printGameBoard(gameBoard);
     }
     //Creating a method to allow the players to make their move.
     public static void makeAMove(String[][] gameBoard)
