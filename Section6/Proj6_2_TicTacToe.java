8b13543ed8500b9f30698c60a25f45e227f5bba2
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-05-31 14:49:46 -0400

    Changed variable name

--- Section6/Proj6_2_TicTacToe.java
+++ Section6/Proj6_2_TicTacToe.java
@@ -74,7 +74,7 @@ public class Proj6_2_TicTacToe {
         //Creating a variable.
         int columnInput;
         int rowInput;
-        String playerOne;
+        String playerTwo;
         //Adding a scanner object.
         Scanner keyboard = new Scanner(System.in);
         //Adding some padding.
