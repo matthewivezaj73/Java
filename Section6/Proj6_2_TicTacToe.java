3138592f4a057cd6409fba722200cbd527eba3ed
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-05-31 14:44:51 -0400

    Added a comment.

--- Section6/Proj6_2_TicTacToe.java
+++ Section6/Proj6_2_TicTacToe.java
@@ -32,6 +32,7 @@ public class Proj6_2_TicTacToe {
         System.out.println("Player with the O counter, it is your turn.");
         //Calling the makeAMove method, so that the user will make a choice.
         makeAMove(gameBoard);
+        //Calling the print gameBoard method to print the game board in it's current state.
     }
     //Creating a method to allow the players to make their move.
     public static void makeAMove(String[][] gameBoard)
