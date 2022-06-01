a3b7cd4daf820c08e891a712d135b1da955b6b9d
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-05-31 20:16:51 -0400

    Removing if and comment as they do nothing.

--- Section6/Proj6_2_TicTacToe.java
+++ Section6/Proj6_2_TicTacToe.java
@@ -33,11 +33,6 @@ public class Proj6_2_TicTacToe {
             makeAMovePlayerTwo(gameBoard);
             //Calling the printGameBoard method.
             printGameBoard(gameBoard);
-            //Creating an if statement to see if there is a winner.
-            if(gameBoard[0][0] == "X")
-            {
-                System.out.println("the first block is an X.");
-            }
         }
     }
     //Creating a method to allow the players to make their move.
