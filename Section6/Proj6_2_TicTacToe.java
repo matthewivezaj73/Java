29fa9db7391a5298a165e6df5a0a8aacf714aafa
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-06-01 07:19:38 -0400

    added a comment.

--- Section6/Proj6_2_TicTacToe.java
+++ Section6/Proj6_2_TicTacToe.java
@@ -33,7 +33,7 @@ public class Proj6_2_TicTacToe {
             makeAMovePlayerTwo(gameBoard);
             //Calling the printGameBoard method.
             printGameBoard(gameBoard);
-            //Need to figure out a way to check if there are 3 of the same variable in a row so that a player wins.
+            //Checking if specific fields are X.
         }
     }
     //Creating a method to allow the players to make their move.
