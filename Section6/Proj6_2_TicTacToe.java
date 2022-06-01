8f9d458d22ea78e84659f75006177955f3f3b2b5
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-05-31 20:14:43 -0400

    Changed text.

--- Section6/Proj6_2_TicTacToe.java
+++ Section6/Proj6_2_TicTacToe.java
@@ -89,7 +89,7 @@ public class Proj6_2_TicTacToe {
         columnInput = keyboard.nextInt();
         //Instructing the user to make a choice for which row they would like to enter their counter in.
         System.out.println("Please enter a 0 to place a counter in the first row.\n"+
-        "Please enter a 2 to place a counter in the second row.\n" +"Please enter a 4 to place your counter in the 3rd row" );
+        "Please enter a 2 to place a counter in the second row.\n" +"Please enter a 4 to place your counter in the third row" );
         //Accepting user input for the row variable.
         rowInput = keyboard.nextInt();
         //Asking the user if they are X or O.
