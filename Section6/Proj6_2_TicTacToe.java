188b2aa90b248fa368b34d2054a3de49e5e269d4
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-05-31 20:32:47 -0400

    Changed print to println.

--- Section6/Proj6_2_TicTacToe.java
+++ Section6/Proj6_2_TicTacToe.java
@@ -63,7 +63,7 @@ public class Proj6_2_TicTacToe {
         System.out.println("Are you X or O?");
         //Accepting user input for the charInput.
         playerOne = keyboard.next();
-        System.out.print(playerOne);
+        System.out.println(playerOne);
         //Notifying the user that we are inserting their character of choice.
         System.out.println("We are now inserting an \'" + playerOne+"\' at position " + rowInput + ", " + columnInput+".");
         //Adding the user's count to the gameboard. Row * Column
