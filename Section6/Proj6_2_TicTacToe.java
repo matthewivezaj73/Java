eabe97f19594926e8c400814735d35ad307a1cc2
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-06-02 11:37:07 -0400

    Added an underscore.

--- Section6/Proj6_2_TicTacToe.java
+++ Section6/Proj6_2_TicTacToe.java
@@ -203,7 +203,7 @@ public class Proj6_2_TicTacToe {
         //Printing out the second horizontal line.
         gameBoard[3][0] = "__";
         gameBoard[3][1] = "__";
-        gameBoard[3][2] = "_";
+        gameBoard[3][2] = "__";
         gameBoard[3][3] = "_";
         gameBoard[3][4] = "_";
     }
