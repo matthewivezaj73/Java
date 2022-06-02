3a9275dccb25375be64c2a2d167d1e6c565b6c37
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-06-02 19:24:48 -0400

    Removed blank line.

--- Section6/Proj6_2_TicTacToe.java
+++ Section6/Proj6_2_TicTacToe.java
@@ -39,7 +39,6 @@ public class Proj6_2_TicTacToe {
         {
             //Incrementing placeCounter.
             placeCounter = placeCounter + 1;
-
             //Creating a variable.
             String myLine = gameBoard[0][0] + gameBoard[0][2]+gameBoard[0][2];
             //Creating a switch statement to go through the possible outcomes.
