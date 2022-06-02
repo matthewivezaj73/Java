ebd27f22224212cb2344bceb46e1b3d3fff18f3c
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-06-01 22:02:11 -0400

    Removed blank line.

--- Section6/Proj6_2_TicTacToe.java
+++ Section6/Proj6_2_TicTacToe.java
@@ -70,7 +70,6 @@ public class Proj6_2_TicTacToe {
                             //Breaking out of the case.
                             break;
                     }
-
                     //Checking if the player X is the winnter.
                     if(myLine.equals("XXX"))
                     {
