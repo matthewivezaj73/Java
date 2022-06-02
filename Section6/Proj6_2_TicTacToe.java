bdc541b0340c48f10941013bf704d13f7ae93391
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-06-02 15:21:30 -0400

    added a comment.

--- Section6/Proj6_2_TicTacToe.java
+++ Section6/Proj6_2_TicTacToe.java
@@ -113,7 +113,7 @@ public class Proj6_2_TicTacToe {
                 //Handling the case where all of the entries are the same.
                 else if(gameBoard[0][0].equals("O") && gameBoard[2][0].equals("O") && gameBoard[4][0].equals("O"))
                 {
-                    
+                    //Printing out a message to the user saying that they won.
                 }
                 //Calling the printGameBoard method.
                 printGameBoard(gameBoard);
