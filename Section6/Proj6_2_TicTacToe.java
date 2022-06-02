639488128b201d6e52b7a54bd23c0e98147a374c
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-06-02 19:43:41 -0400

    Reducing _.

--- Section6/Proj6_2_TicTacToe.java
+++ Section6/Proj6_2_TicTacToe.java
@@ -248,7 +248,7 @@ public class Proj6_2_TicTacToe {
         //Adding underscores.
         //Printing out the first horizontal line.
         gameBoard[1][0] = "_";
-        gameBoard[1][1] = "__";
+        gameBoard[1][1] = "_";
         gameBoard[1][2] = "__";
         gameBoard[1][3] = "__";
         gameBoard[1][4] = "__";
