a7803cb5b8bb98197ccb43896b2cf5293820890f
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-06-02 19:18:33 -0400

    Removed blank line.

--- Section6/Proj6_2_TicTacToe.java
+++ Section6/Proj6_2_TicTacToe.java
@@ -100,7 +100,6 @@ public class Proj6_2_TicTacToe {
         }
         //Creating another while loop.
         while(placeCounter < 100)
-
         {
             //Printing the gameBoard.
             printGameBoard(gameBoard);
