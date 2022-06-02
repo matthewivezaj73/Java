9e35162cb24aa8055bc47ecaa5ee3d816ca2df51
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-06-02 11:33:16 -0400

    Removed blank line.

--- Section6/Proj6_2_TicTacToe.java
+++ Section6/Proj6_2_TicTacToe.java
@@ -43,7 +43,6 @@ public class Proj6_2_TicTacToe {
             //Checking if specific fields are X.
             for (int myInt = 0; myInt < 8; myInt++)
             {
-       
                     //Creating a variable.
                     String myLine = gameBoard[0][0] + gameBoard[0][2]+gameBoard[0][2];
                     //Creating a switch statement to go through the possible outcomes.
