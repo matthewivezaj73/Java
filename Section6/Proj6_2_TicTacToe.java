d5556c0d7425ff6530df4c0bed9a992142fff598
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-06-02 07:08:17 -0400

    Removed code.

--- Section6/Proj6_2_TicTacToe.java
+++ Section6/Proj6_2_TicTacToe.java
@@ -61,8 +61,6 @@ public class Proj6_2_TicTacToe {
                         case 1:
                             //Creating a winning line.
                             myLine = gameBoard[2][0] + gameBoard[2][2] + gameBoard[2][4];
-                            //Breaking out of the case.
-                            break;
                         //Creating the third case.
                         case 2:
                             //Creating a winning line.
