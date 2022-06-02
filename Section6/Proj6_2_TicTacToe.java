325f6db89641896ee0ca3f2aab87a80274e2f308
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-06-02 13:50:08 -0400

    added a comment.

--- Section6/Proj6_2_TicTacToe.java
+++ Section6/Proj6_2_TicTacToe.java
@@ -76,6 +76,7 @@ public class Proj6_2_TicTacToe {
                     {
                         //Printing out a message to the user saying that they won.
                         System.out.println("Congratulations, Player One has won the game!");
+                        //Breaking out of the loop.
                     }
                     if(gameBoard[0][0].equals("X") && gameBoard[2][0].equals("X") && gameBoard[4][0].equals("X"))
                     {
