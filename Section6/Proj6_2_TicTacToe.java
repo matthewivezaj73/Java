4d1f153621d44e199a30b8d6e997b43bdff422dc
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-06-02 13:37:36 -0400

    Printing a line to the user.

--- Section6/Proj6_2_TicTacToe.java
+++ Section6/Proj6_2_TicTacToe.java
@@ -81,6 +81,7 @@ public class Proj6_2_TicTacToe {
                     else if(gameBoard[2][0].equals("X") && gameBoard[2][2].equals("X") && gameBoard[2][4].equals("X"))
                     {
                         //Printing out a message to the user saying that they won.
+                        System.out.println("Congratulations, Player One has won the game!");
                     }
                 }
             //Adding catch for the exception of what to do when an error is found.
