74d782b4ac5c9fb96e991670417ffd63e89794bf
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-06-02 19:37:44 -0400

    Added myspace as the assignment variable.

--- Section6/Proj6_2_TicTacToe.java
+++ Section6/Proj6_2_TicTacToe.java
@@ -245,7 +245,7 @@ public class Proj6_2_TicTacToe {
         gameBoard[1][3] = mySpace;
         gameBoard[1][4] = mySpace;
         //Printing out the second horizontal line.
-        gameBoard[3][0] = "__";
+        gameBoard[3][0] = mySpace;
         gameBoard[3][1] = "__";
         gameBoard[3][2] = "__";
         gameBoard[3][3] = "__";
