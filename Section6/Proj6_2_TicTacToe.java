a1c554a0bbf8afb11b9ddc9cc3f97b6be3e9b388
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-06-03 19:17:52 -0400

    Created else if.

--- Section6/Proj6_2_TicTacToe.java
+++ Section6/Proj6_2_TicTacToe.java
@@ -129,6 +129,7 @@ public class Proj6_2_TicTacToe {
                     break;
                 }
                 //Handling the case where all of the entries are in the same alignment.
+                else if(gameBoard[4][0].equals("O") && gameBoard[2][2].equals("O") && gameBoard[0][4].equals("O"))
             }
             //Creating a catch block.
             catch(Exception e)
