996388f472ce79bddc454f21fa4b1e6c3e39e0db
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-06-03 08:38:58 -0400

    Changed to else if.

--- Section6/Proj6_2_TicTacToe.java
+++ Section6/Proj6_2_TicTacToe.java
@@ -71,7 +71,7 @@ public class Proj6_2_TicTacToe {
                     //Breaking out of the loop.
                     break;
                 }
-                if(gameBoard[0][0].equals("X") && gameBoard[2][0].equals("X") && gameBoard[4][0].equals("X"))
+                else if(gameBoard[0][0].equals("X") && gameBoard[2][0].equals("X") && gameBoard[4][0].equals("X"))
                 {
                     //Printing out a message to the user saying that they won.
                     System.out.println("Congratulations, Player One has won the game!");
