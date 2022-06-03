c623f83434f1afac7afdb48d1365337b53955664
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-06-03 19:35:17 -0400

    added a comment.

--- Section6/Proj6_2_TicTacToe.java
+++ Section6/Proj6_2_TicTacToe.java
@@ -147,7 +147,7 @@ public class Proj6_2_TicTacToe {
                 //Handling the case where all of the entries are in the same alignment.
                 else if(gameBoard[4][0].equals("O") && gameBoard[4][2].equals("O") && gameBoard[4][4].equals("O"))
                 {
-                    
+                    //Printing out a messge to the user saying that they won.
                 }
             }
             //Creating a catch block.
