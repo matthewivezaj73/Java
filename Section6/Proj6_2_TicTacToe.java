849250e320b6f6dcbc2bf130664cca9539dcec29
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-06-03 08:46:49 -0400

    Added a statement for printing.

--- Section6/Proj6_2_TicTacToe.java
+++ Section6/Proj6_2_TicTacToe.java
@@ -115,6 +115,7 @@ public class Proj6_2_TicTacToe {
                 else if(gameBoard[4][0].equals("O") && gameBoard[4][2].equals("O" ) && gameBoard[4][4].equals("O"))
                 {
                     //Printing out a message to the user saying that they won.
+                    System.out.println("Congratulations, Player Two has won the game!");
                 }
             }
             //Adding catch for the exception of what to do when an error is found.
