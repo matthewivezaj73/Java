c8f4105797fe3bb932a7c0d458369d2975c8e704
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-06-03 08:40:02 -0400

    Added else in front of if.

--- Section6/Proj6_2_TicTacToe.java
+++ Section6/Proj6_2_TicTacToe.java
@@ -87,7 +87,7 @@ public class Proj6_2_TicTacToe {
                     break;
                 }
                 //Handling the case where all of the entries are the same.
-                if(gameBoard[0][0].equals("O") && gameBoard[0][2].equals("O") && gameBoard[0][4].equals("O"))
+                else if(gameBoard[0][0].equals("O") && gameBoard[0][2].equals("O") && gameBoard[0][4].equals("O"))
                 {
                     //Printing out a message to the user saying that they won.
                     System.out.println("Congratulations, Player Two has won the game!");
