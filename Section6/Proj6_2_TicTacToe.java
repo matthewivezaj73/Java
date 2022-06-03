d9f8939b53042169fc5b713d7bc28dca0e0bede4
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-06-03 14:02:21 -0400

    Added code to try.

--- Section6/Proj6_2_TicTacToe.java
+++ Section6/Proj6_2_TicTacToe.java
@@ -95,6 +95,38 @@ public class Proj6_2_TicTacToe {
                     //Breaking out of the loop.
                     break;
                 }
+                //Handling the case where all of the entries are in the same alignment.
+                else if(gameBoard[0][0].equals("O") && gameBoard[0][2].equals("O") && gameBoard[0][4].equals("O"))
+                {
+                    //Printing out a message to the user saying that they won.
+                    System.out.println("Congratulations, Player Two has won the game!");
+                    //Breaking out of the loop.
+                    break;
+                }
+                //Handling the case where all of the entries are in the same alignment.
+                else if(gameBoard[0][0].equals("O") && gameBoard[2][0].equals("O") && gameBoard[4][0].equals("O"))
+                {
+                    //Printing out a message to the user saying that they won.
+                    System.out.println("Congratulations, Player Two has won the game!");
+                    //Breaking out of the loop.
+                    break;
+                }
+                //Handling the case where all of the entries are in the same alignment.
+                else if(gameBoard[2][0].equals("O") && gameBoard[2][2].equals("O") && gameBoard[2][4].equals("O"))
+                {
+                    //Printing out a message to the user saying that they won.
+                    System.out.println("Congratulations, Player Two has won the game!");
+                    //Breaking out of the loop.
+                    break;
+                }
+                //Handling the case where all of the entries are in the same alignment.
+                else if(gameBoard[4][0].equals("O") && gameBoard[4][2].equals("O" ) && gameBoard[4][4].equals("O"))
+                {
+                    //Printing out a message to the user saying that they won.
+                    System.out.println("Congratulations, Player Two has won the game!");
+                    //Breaking out of the loop.
+                    break;
+                }
             }
             //Adding catch for the exception of what to do when an error is found.
             catch(Exception e)
