50cdda80a3953cfd98658e51f425224c720952c2
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-06-03 19:18:02 -0400

    Added curly braces.

--- Section6/Proj6_2_TicTacToe.java
+++ Section6/Proj6_2_TicTacToe.java
@@ -130,6 +130,9 @@ public class Proj6_2_TicTacToe {
                 }
                 //Handling the case where all of the entries are in the same alignment.
                 else if(gameBoard[4][0].equals("O") && gameBoard[2][2].equals("O") && gameBoard[0][4].equals("O"))
+                {
+                    
+                }
             }
             //Creating a catch block.
             catch(Exception e)
