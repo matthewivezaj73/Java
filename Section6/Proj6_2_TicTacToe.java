fbb9ed507b7fc39d3cb929aff81c6be2b829454f
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-06-04 13:41:33 -0400

    Changed param

--- Section6/Proj6_2_TicTacToe.java
+++ Section6/Proj6_2_TicTacToe.java
@@ -116,7 +116,7 @@ public class Proj6_2_TicTacToe {
                 break;
             }
             //Handling the case where all of the entries are in the same alignment.
-            else if(gameBoard[4][2].equals("O"))
+            else if(gameBoard[4][4].equals("O"))
             {
                 //Printing out a message to the user saying that they won.
                 System.out.println(gameBoard[4][0]);
