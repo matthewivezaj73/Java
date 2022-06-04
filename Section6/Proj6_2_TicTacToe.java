4600bf32340fa8c681cb032559fac89f92b5f864
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-06-04 10:37:50 -0400

    Pasted code for testing.

--- Section6/Proj6_2_TicTacToe.java
+++ Section6/Proj6_2_TicTacToe.java
@@ -48,6 +48,14 @@ public class Proj6_2_TicTacToe {
                 break;
             }
             //Handling the case where all of the entries are in the same alignment.
+            else if((gameBoard[4][0].equals("X") && gameBoard[4][2].equals("X") && gameBoard[4][4].equals("X")) || (gameBoard[4][0].equals("O") && gameBoard[4][2].equals("O") && gameBoard[4][4].equals("O")))
+            {
+                //Printing out a message to the user saying that they won.
+                System.out.println("Congratulations, Player One has won the game!");
+                //Breaking out of the loop.
+                break;
+            }
+            //Handling the case where all of the entries are in the same alignment.
             else if((gameBoard[0][0].equals("X") && gameBoard[2][0].equals("X") && gameBoard[4][0].equals("X")) || (gameBoard[0][0].equals("O") && gameBoard[2][0].equals("O") && gameBoard[4][0].equals("O")))
             {
                 //Printing out a message to the user saying that they won.
