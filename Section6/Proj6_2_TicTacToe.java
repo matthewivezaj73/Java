787045d4e0810ef0870e7d22623d3d75caa2a738
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-06-04 17:21:29 -0400

    added a comment.

--- Section6/Proj6_2_TicTacToe.java
+++ Section6/Proj6_2_TicTacToe.java
@@ -33,6 +33,7 @@ public class Proj6_2_TicTacToe {
         //Creating a while loop.
         while(placeCounter < 100)
         {
+            //Printing a piece of the gameboard for testing.
             //Incrementing placeCounter.
             placeCounter = placeCounter + 1;
             //Calling the makeAMovePlayerOne method.
