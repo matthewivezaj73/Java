1205a4a2e618d010830e606b9b89a60a8457e207
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-06-04 09:42:44 -0400

    Commented struct out.

--- Section6/Proj6_2_TicTacToe.java
+++ Section6/Proj6_2_TicTacToe.java
@@ -81,14 +81,14 @@ public class Proj6_2_TicTacToe {
                     //Breaking out of the loop.
                     break;
                 }
-                //Handling the case where all of the entries are in the same alignment.
-                else if
-                {
-                    //Printing out a message to the user saying that they won.
-                    System.out.println("Congratulations, Player One has won the game!");
-                    //Breaking out of the loop.
-                    break;
-                }
+                // //Handling the case where all of the entries are in the same alignment.
+                // else if
+                // {
+                //     //Printing out a message to the user saying that they won.
+                //     System.out.println("Congratulations, Player One has won the game!");
+                //     //Breaking out of the loop.
+                //     break;
+                // }
             }
             //Adding catch for the exception of what to do when an error is found.
             catch(Exception e)
