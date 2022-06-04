a36f304cfd99830dbf65f0764436b468ac6e2b17
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-06-04 17:16:29 -0400

    Pasted code in comment.

--- Section6/Proj6_2_TicTacToe.java
+++ Section6/Proj6_2_TicTacToe.java
@@ -77,6 +77,7 @@ public class Proj6_2_TicTacToe {
                 //Breaking out of the loop.
                 break;
             }
+            // && gameBoard[4][2].equals("X") && gameBoard[4][4].equals("X") || gameBoard[4][0].equals("O") && gameBoard[4][2].equals("O") && gameBoard[4][4].equals("O")
             //Handling the case where all of the entries are in the same alignment.
             else if(gameBoard[4][0].equals("X") )
             {
