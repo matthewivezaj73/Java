91780d8559c9851059ec065f1ccbbae8cf63a8c5
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-06-04 13:21:47 -0400

    Removed code for testing.

--- Section6/Proj6_2_TicTacToe.java
+++ Section6/Proj6_2_TicTacToe.java
@@ -143,7 +143,7 @@ public class Proj6_2_TicTacToe {
             //FOR SOME REASON IT IS NOT READING THE ONE BELOW, I NEED TO INVESTIGATE WHY THIS MAY BE HAPPENING.
 
             //Handling the case where all of the entries are in the same alignment.
-            else if(gameBoard[4][0].equals("O") && gameBoard[4][2].equals("O") && gameBoard[0][0].equals("O"))
+            else if(gameBoard[0][0].equals("O"))
             {
                 //Printing out a messge to the user saying that they won.
                 System.out.println("Congratulations, Player Two has won the game!");
