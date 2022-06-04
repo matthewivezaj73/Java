f671e9b9c56b1c8e81aa74362c599e1881f4767e
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-06-04 19:49:19 -0400

    Added brackets.

--- Section6/Proj6_2_TicTacToe.java
+++ Section6/Proj6_2_TicTacToe.java
@@ -87,7 +87,7 @@ public class Proj6_2_TicTacToe {
                 break;
             }
             //Creating a for loop.
-            for(String x: gameBoard)
+            for(String[] x: gameBoard)
             {
 
             }
