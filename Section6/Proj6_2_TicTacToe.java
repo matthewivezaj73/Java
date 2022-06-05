95f96be46e0bddacf257abcc833f24eed6078870
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-06-05 17:23:00 -0400

    added a comment.

--- Section6/Proj6_2_TicTacToe.java
+++ Section6/Proj6_2_TicTacToe.java
@@ -65,6 +65,7 @@ public class Proj6_2_TicTacToe {
             //Handling the case where all of the entries are in the same alignment.
             else if(gameBoard[2][2].equals("X") || (gameBoard[2][4].equals("O") ))
             {
+                //Adding an if statement to check if gameBoard[2][0] is valid.
                 //Printing out a message to the user saying that they won.
                 System.out.println("Congratulations, Player One has won the game!");
                 //Breaking out of the loop.
