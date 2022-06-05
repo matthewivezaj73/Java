b5a2e436c15a596266467bff4418e0a1071d83d6
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-06-05 18:50:33 -0400

    changed params.

--- Section6/Proj6_2_TicTacToe.java
+++ Section6/Proj6_2_TicTacToe.java
@@ -89,7 +89,7 @@ public class Proj6_2_TicTacToe {
                 break;
             }
             //Handling the case where all of the entries are in the same alignment.
-            else if((gameBoard[0][0] == "X" && gameBoard[0][2].equals("X")) && gameBoard[0][4] == "X" || (gameBoard[0][0].equals("O") && gameBoard[0][2].equals("O") && gameBoard[0][4].equals("O")))
+            else if((gameBoard[4][4] == "X" && gameBoard[4][2].equals("X")) && gameBoard[0][4] == "X" || (gameBoard[0][0].equals("O") && gameBoard[0][2].equals("O") && gameBoard[0][4].equals("O")))
             {
                 //Printing out a message to the user saying that they won.
                 System.out.println("Congratulations, Player One won the game!");
