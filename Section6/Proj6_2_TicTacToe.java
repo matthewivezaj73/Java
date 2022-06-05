8cd51875dde0545d804122c5c624604e38aea08e
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-06-05 15:33:03 -0400

    Added parenthesis.

--- Section6/Proj6_2_TicTacToe.java
+++ Section6/Proj6_2_TicTacToe.java
@@ -54,7 +54,7 @@ public class Proj6_2_TicTacToe {
                 break;
             }
             //Handling the case where all of the entries are in the same alignment.
-            else if(gameBoard[2][0].equals("X") && gameBoard[2][2].equals("X") && gameBoard[2][4].equals("X") || (gameBoard[2][2].equals("O") ))
+            else if(gameBoard[2][0].equals("X") && (gameBoard[2][2].equals("X") && gameBoard[2][4].equals("X")) || (gameBoard[2][2].equals("O") ))
             {
                 //Printing out a message to the user saying that they won.
                 System.out.println("Congratulations, Player One has won the game!");
