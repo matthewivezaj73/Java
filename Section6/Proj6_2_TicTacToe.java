c098bcc90b892f161f05ee515d7be864aebad0ed
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-06-06 19:50:45 -0400

    Reusing old cod.e

--- Section6/Proj6_2_TicTacToe.java
+++ Section6/Proj6_2_TicTacToe.java
@@ -90,10 +90,10 @@ public class Proj6_2_TicTacToe {
                 break;
             }
             //Handling the case where all of the entries are in the same alignment.
-            else if((gameBoard[4][2] == "X")&&(gameBoard[4][4].equals("X")))
+            if((gameBoard[0][0] == "X" && gameBoard[0][2].equals("X")) && gameBoard[0][4] == "X" || (gameBoard[0][0].equals("O") && gameBoard[0][2].equals("O") && gameBoard[0][4].equals("O")))
             {
                 //Printing out a message to the user saying that they won.
-                System.out.println("Congratulations, Player One won the game!");
+                System.out.println("Congratulations, Player One has won the game!");
                 //Breaking out of the loop.
                 break;
             }
