55ab7c5a18e920778a2cdf4204d37d7fe4a23fe7
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-06-07 07:04:40 -0400

    Created a new file and pasted code.

---
+++ Section7/HouseDemo.java
@@ -0,0 +1,45 @@
+//Creating a public class.
+public class House {
+    //Creating private variables.
+    private int numStories;
+    private int numWindows;
+    private String color;
+
+    //Creating a method.
+    public int getNumStories()
+    {
+        //Returning one of our private variables.
+        return numStories;
+    }
+    //Creating a method.
+    public void setNumStories()
+    {
+        //Referencing the current object.
+        this.numStories = numStories;
+    }
+
+    //Creating a method.
+    public int getNumWindows()
+    {
+        //Returning numWindows.
+        return numWindows;
+    }
+    //Creating a method.
+    public void setNumWindows(int numWindows)
+    {
+        //Referencing the current object.
+        this.numWindows = numWindows;
+    }
+    //Creating a method.
+    public String getColor()
+    {
+        //Returning color.
+        return color;
+    }
+    //Creating a method.
+    public void setColor(String color)
+    {
+        //Referencing the current object.
+        this.color = color;
+    }
+}
