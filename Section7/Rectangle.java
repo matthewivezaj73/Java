c1a4cc137773e26a41550e0a81ac45d69f5ce143
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-06-08 19:35:01 -0400

    Removed main method.

--- Section7/Rectangle.java
+++ Section7/Rectangle.java
@@ -1,8 +1,5 @@
 //Creating a public class.
 public class Rectangle {
-    //Creating a public method.
-    public static void main(String[] args)
-    {
         //Creating private variables.
         private double length;
         private double width;
