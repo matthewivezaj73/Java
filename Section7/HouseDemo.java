ce6c3e9b47da69cea5a0b6e9c017b0afcc0c6cc5
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-06-08 07:39:32 -0400

    Called printHouseData and passed myHouse.

--- Section7/HouseDemo.java
+++ Section7/HouseDemo.java
@@ -36,5 +36,6 @@ public class HouseDemo {
         //Printing a method to the user.
         System.out.println("Your house is " + yourHouse.getColor() + " and has " + yourHouse.getNumStories() + " stories and " + yourHouse.getNumWindows() + " windows.");
         //Calling the newly created method named printHouseData.
+        printHouseData(myHouse);
     }
 }
