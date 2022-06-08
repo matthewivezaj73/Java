f49b524eecca292345e022dc14d1fc6797911a64
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-06-07 21:23:38 -0400

    Created first line of method.

--- Section7/HouseDemo.java
+++ Section7/HouseDemo.java
@@ -1,6 +1,7 @@
 //Creating a public class.
 public class HouseDemo {
     //Creating a method.
+    public printHouseData()
     //Creating a method.
     public static void main(String[]args)
     {
