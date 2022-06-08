bbdeb515ddd32a780c213167863d4492e15db7a8
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-06-08 07:26:06 -0400

    removed the return.

--- Section7/HouseDemo.java
+++ Section7/HouseDemo.java
@@ -5,7 +5,6 @@ public class HouseDemo {
     {
         String color = "Green";
         //Returning the variable passed.
-        return color;
     }
     //Creating a method.
     public static void main(String[]args)
