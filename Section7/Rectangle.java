3ba39eced690d075cfc5701f8972ea1e554c02b7
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-06-08 19:35:22 -0400

    unindented code.

--- Section7/Rectangle.java
+++ Section7/Rectangle.java
@@ -1,21 +1,21 @@
 //Creating a public class.
 public class Rectangle {
-        //Creating private variables.
-        private double length;
-        private double width;
-        //Creating a static field.
-        private static int numRectangles;
-        //Creating a constructor.
-        public Rectangle()
-        {
+    //Creating private variables.
+    private double length;
+    private double width;
+    //Creating a static field.
+    private static int numRectangles;
+    //Creating a constructor.
+    public Rectangle()
+    {
 
-        }
-        //Creating another constructor with passed arguments.
-        public Rectangle(double length, double width)
-        {
-            //accessing only the variable by using this.
-            this.length = length;
-            this.width = width;
-        }
     }
+    //Creating another constructor with passed arguments.
+    public Rectangle(double length, double width)
+    {
+        //accessing only the variable by using this.
+        this.length = length;
+        this.width = width;
+    }
+}
 
