ec8e519425c9592f12c4f99425c1734aa6361891
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-06-09 19:41:30 -0400

    Removed blank lines.

--- Section7/Book.java
+++ Section7/Book.java
@@ -1,16 +1,13 @@
 //Creating java class.
 public class Book {
-
     //Creating private variables.
     private String author;
     private String title;
     private String genre;
     private int numPages;
-
     //Creating a constructor.
     public Book(String Author, String Title, String genre, int numPages)
     {
 
     }
-
 }
