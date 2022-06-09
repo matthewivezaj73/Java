64a1f11d5db7a62d925d95448adb8fbed876577f
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-06-09 19:41:08 -0400

    Unindented code.

--- Section7/Book.java
+++ Section7/Book.java
@@ -2,16 +2,16 @@
 public class Book {
     //Creating a main method.
 
-        //Creating private variables.
-        private String author;
-        private String title;
-        private String genre;
-        private int numPages;
+    //Creating private variables.
+    private String author;
+    private String title;
+    private String genre;
+    private int numPages;
 
-        //Creating a constructor.
-        public Book(String Author, String Title, String genre, int numPages)
-        {
+    //Creating a constructor.
+    public Book(String Author, String Title, String genre, int numPages)
+    {
 
-        }
+    }
 
 }
