3d9d16204d309c0cccd155ea1a59aa4291435f6d
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-06-09 07:44:52 -0400

    added a comment.

--- Section7/RectangleDemo.java
+++ Section7/RectangleDemo.java
@@ -15,5 +15,6 @@ public class RectangleDemo {
         System.out.println("After r3 declared: " + Rectangle.getNumRectangles());
         //Assigning r3 a value.
         r3 = new Rectangle(2.5, 4);
+        //Printing out the current state of the number of rectangles.
     }
 }
