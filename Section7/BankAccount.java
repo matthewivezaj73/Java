5f203dd4d5339d9af31f771e6ac9af73db415a6e
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-06-11 16:16:39 -0400

    Changed datatype.

--- Section7/BankAccount.java
+++ Section7/BankAccount.java
@@ -19,7 +19,7 @@ public class BankAccount {
         BankAccount.deposit = deposit;
     }
     //Creating a constructor to represent the act of checking your balance.
-    public double balance()
+    public int balance()
     {
         //Returning the current balance.
         return balance;
