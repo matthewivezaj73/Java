a6ce1ca9e90ad0c96ed3b8be647ee521d10ff77d
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-06-11 16:57:45 -0400

    Removed comments and other banking objects.

--- Section7/BankAccountDemo.java
+++ Section7/BankAccountDemo.java
@@ -10,12 +10,7 @@ public class BankAccountDemo {
     {
         //Creating a bank account object.
         BankAccount myBank = new BankAccount("Matthew", 232324, 4323, 7324);
-        //Creating a bank account object.
-        BankAccount yourBank = new BankAccount("Suzy", 432864, 3221, 42433);
-        //Creating a bank account object.
-        BankAccount hisBank = new BankAccount("Sam",762442, 32423, 43243);
-        //Creating a bank account object.
-        BankAccount herBank = new BankAccount("Taylor", 89423874, 298347, 878432);
+        
         //Printing the details for myBank.
         myBank.displayBankingDetails();
         //Printing the details for yourBank.
