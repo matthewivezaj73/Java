ad1f6384c8f75b39f8cbe59a88e8e988f3ed0382
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-06-11 15:52:06 -0400

    Creating a bank account object

--- Section7/BankAccountDemo.java
+++ Section7/BankAccountDemo.java
@@ -11,5 +11,6 @@ public class BankAccountDemo {
         //Creating a bank account object.
         BankAccount myBank = new BankAccount(232324.00, 4323.32, 7324.32);
         //Creating a bank account object.
+        BankAccount yourBank = new BankAccount(432864.32, 3221.34, 42433.14);
     }
 }
