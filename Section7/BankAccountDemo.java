483a44b49e690a9edc82473c49bb1fb5d94e4bfe
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-06-11 17:12:18 -0400

    Grabbing account owner's name.

--- Section7/BankAccountDemo.java
+++ Section7/BankAccountDemo.java
@@ -11,6 +11,7 @@ public class BankAccountDemo {
         //Creating a bank account object.
         BankAccount myBank = new BankAccount("Matthew", 0, 4323, 7324);
         //Printing the account owner's name.
+        myBank.getOwner("Matthew");
         //Printing the balance.
         System.out.println("You are starting with, " + myBank.balance(0));
         //Printing the details for myBank.
