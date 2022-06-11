4f4744ffa21d55699c79020d2d5e89fd88659b31
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-06-11 15:49:24 -0400

    changed variable name

--- Section7/BankAccount.java
+++ Section7/BankAccount.java
@@ -10,7 +10,7 @@ public class BankAccount {
     private static double withdrawal;
     private static double deposit;
     //Creating a public method.
-    public BankAccount(double balance, double e, double f)
+    public BankAccount(double balance, double withdrawal, double f)
     {
         //Initializing variables.
         BankAccount.balance = d;
