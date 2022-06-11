7005c5508f2b02897edacce4bbbafb6b6416c382
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-06-11 10:11:02 -0400

    Created a float object.

--- Section7/BankAccount.java
+++ Section7/BankAccount.java
@@ -1,6 +1,7 @@
 //Creating a public class.
 public class BankAccount {
     //Creating private variables.
+    private static float balance;
     //Creating a public method.
     public static void main(String[] args)
     {
