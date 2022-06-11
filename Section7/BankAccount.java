79d2ebdd4e591ca514e64e693c465c640847344a
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-06-11 10:10:30 -0400

    added a comment.

--- Section7/BankAccount.java
+++ Section7/BankAccount.java
@@ -1,8 +1,9 @@
 //Creating a public class.
 public class BankAccount {
+    //Creating private variables.
     //Creating a public method.
     public static void main(String[] args)
     {
-        
+
     }
 }
