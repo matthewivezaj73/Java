b636e2238fdacbbe6a357177337de125cad0f894
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-06-11 19:43:29 -0400

    Changed data types.

--- Section7/BankAccountDemo.java
+++ Section7/BankAccountDemo.java
@@ -23,9 +23,9 @@ public class BankAccountDemo {
         //Printing the balance.
         System.out.print("Please enter the value you would like to withdraw from your bank account:\t");
         //Accepting user input.
-        double myWithdrawal = keyboard.nextDouble();
+        int myWithdrawal = keyboard.nextInt();
         //Printing the money left over after a withdrawal.
-        System.out.println((myBank.withdrawMoney(2)));
+        System.out.println((myBank.withdrawMoney(myWithdrawal)));
 
     }
 }
