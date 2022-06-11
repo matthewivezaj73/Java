9569ddbb11fdf432746c1986a6ac3619df87b6ed
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-06-11 17:17:02 -0400

    added a comment.

--- Section7/BankAccount.java
+++ Section7/BankAccount.java
@@ -38,9 +38,10 @@ public class BankAccount {
         System.out.println("You are now depositing, " + deposit);
         //Printing out the amount the user would like to withdraw.
         System.out.println("You are now withdrawing, "+withdrawal);
+        //Checking if balance - withdrawal is greater than 0.
         if(balance - withdrawal < 0)
         {
-            
+
         }
         //Checking if withdrawal is a number greater than 0.
         if((withdrawal > 0) && (deposit > 0))
