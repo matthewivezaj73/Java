453ee673af03acc0e370d45c7f65043676b52dd6
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-06-11 15:48:49 -0400

    Changed data type.

--- Section7/BankAccount.java
+++ Section7/BankAccount.java
@@ -6,7 +6,7 @@
 //Creating a public class.
 public class BankAccount {
     //Creating private variables.
-    private static float balance;
+    private static double balance;
     private static float withdrawal;
     private static float deposit;
     //Creating a public method.
