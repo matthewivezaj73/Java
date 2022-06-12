732438774b76a453346812b8a74fb2f225d3d330
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-06-12 13:07:45 -0400

    Created a private variable.

--- Section7/icecream/IceCream.java
+++ Section7/icecream/IceCream.java
@@ -7,6 +7,7 @@ package icecream;
 //Creating a public class.
 public class IceCream {
     //Creating private variables.
+    private String name;
     private int numScoops;
     private String toppings[];
 }
