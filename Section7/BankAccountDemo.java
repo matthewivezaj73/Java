808ad67dd404eb4a52a75eb99559515bba02da29
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-06-11 20:31:44 -0400

    Pasted and indented code.

--- Section7/BankAccountDemo.java
+++ Section7/BankAccountDemo.java
@@ -21,40 +21,40 @@ public class BankAccountDemo {
         //Creating a while loop.
         while(!myFlag)
         {
-            
-        }
-        //Creating a try/catch block.
-        try
-        {
-            //Allowing user to select an amount to deposit.
-            double depositMoney = keyboard.nextDouble();
-        }
-        catch(Exception e)
-        {
-            //Printing a message to the user.
-            System.out.println("Sorry, but that is not a valid entry.");
-        }
-        //Assigning a variable to the act of depositing money into the bank.
-        int my_balance = myBank.deposit(43243);
-        //Printing the current amount the user has.
-        System.out.println(my_balance);
-        //Printing the account owner's name.
-        System.out.println("The account owner is, " + myBank.getOwner("Matthew"));
-        //Printing the balance.
-        System.out.print("Please enter the value you would like to withdraw from your bank account:\t");
-        //Creating a try/finally block.
-        try 
-        {
-            //Accepting user input.
-            int myWithdrawal = keyboard.nextInt();
-            //Printing the money left over after a withdrawal.
-            System.out.println((myBank.withdrawMoney(my_balance, myWithdrawal)));
-        }
-        //Catching the exception where an invalid value has been entered.
-        catch(Exception E)
-        {
-            //Printing out that the operation may not be completed.
-            System.out.println("Sorry, but the entry was not valid, please try again! ");
+            //Creating a try/catch block.
+            try
+            {
+                //Allowing user to select an amount to deposit.
+                double depositMoney = keyboard.nextDouble();
+            }
+            catch(Exception e)
+            {
+                //Printing a message to the user.
+                System.out.println("Sorry, but that is not a valid entry.");
+            }
+            //Assigning a variable to the act of depositing money into the bank.
+            int my_balance = myBank.deposit(43243);
+            //Printing the current amount the user has.
+            System.out.println(my_balance);
+            //Printing the account owner's name.
+            System.out.println("The account owner is, " + myBank.getOwner("Matthew"));
+            //Printing the balance.
+            System.out.print("Please enter the value you would like to withdraw from your bank account:\t");
+            //Creating a try/finally block.
+            try 
+            {
+                //Accepting user input.
+                int myWithdrawal = keyboard.nextInt();
+                //Printing the money left over after a withdrawal.
+                System.out.println((myBank.withdrawMoney(my_balance, myWithdrawal)));
+            }
+            //Catching the exception where an invalid value has been entered.
+            catch(Exception E)
+            {
+                //Printing out that the operation may not be completed.
+                System.out.println("Sorry, but the entry was not valid, please try again! ");
+            }
+
         }
         
     }
