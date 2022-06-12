eabb6f12d08862081bdb1f876231d5a098369fd7
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-06-11 20:43:58 -0400

    Setting myFlag to true.

--- Section7/BankAccountDemo.java
+++ Section7/BankAccountDemo.java
@@ -49,6 +49,7 @@ public class BankAccountDemo {
                 int myWithdrawal = keyboard.nextInt();
                 //Printing the money left over after a withdrawal.
                 System.out.println((myBank.withdrawMoney(my_balance, myWithdrawal)));
+                myFlag = true;
             }
             //Catching the exception where an invalid value has been entered.
             catch(Exception E)
