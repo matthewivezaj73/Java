d68edfd4acba462a3632a30d476f270a3c2bb792
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-06-12 20:28:13 -0400

    added a comment.

--- Section7/icecream/IceCream.java
+++ Section7/icecream/IceCream.java
@@ -19,6 +19,7 @@ public class IceCream {
     {
         //Adding the single topping to the list.
         toppings.add(topping);
+        //Returning the topping.
     }
     //Creating a constructor to get the cost.
     public int getCost(int Cost)
