4b320d2829bf12ad6564ee25a30c6e007d5175ed
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-06-12 20:28:48 -0400

    Returning the topping.

--- Section7/icecream/IceCream.java
+++ Section7/icecream/IceCream.java
@@ -20,6 +20,7 @@ public class IceCream {
         //Adding the single topping to the list.
         toppings.add(topping);
         //Returning the topping.
+        return topping;
     }
     //Creating a constructor to get the cost.
     public int getCost(int Cost)
