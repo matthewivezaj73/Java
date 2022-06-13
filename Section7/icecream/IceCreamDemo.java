240ee3b8ef8b4f85e51c2a96b58b54ebb4ab5623
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-06-13 19:44:31 -0400

    Commit

--- Section7/icecream/IceCreamDemo.java
+++ Section7/icecream/IceCreamDemo.java
@@ -22,6 +22,7 @@ public class IceCreamDemo {
         while(not_done != true)
         {
             //Asking the user what type of topping they would like to add.
+            
             //Adding a topping to the icecream.
             String myTopping = Sherbet.addTopping("lemon");
             //Printing out the topping.
