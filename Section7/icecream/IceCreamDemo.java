f3b89c52b5da55b948cb0b4a8717c54eb4db85da
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-06-13 20:25:11 -0400

    Undid changes.

--- Section7/icecream/IceCreamDemo.java
+++ Section7/icecream/IceCreamDemo.java
@@ -20,14 +20,13 @@ public class IceCreamDemo {
         IceCream Sherbet = new IceCream();
         //Creating a flag.
         String userContinue = "";
-        //Asking the user what type of topping they would like to add.
-        System.out.println("Please enter the topping you would like to add:\t");
-        //Allowing the user to enter a topping.
-        selectedTopping = keyboard.nextLine();
         // //Creating a while loop.
         while(userContinue != "No")
         {
-
+            //Asking the user what type of topping they would like to add.
+            System.out.println("Please enter the topping you would like to add:\t");
+            //Allowing the user to enter a topping.
+            selectedTopping = keyboard.nextLine();
             //Adding a topping to the icecream.
             String myTopping = Sherbet.addTopping(selectedTopping);
             //Printing out the topping.
