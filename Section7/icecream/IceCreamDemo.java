a2e0d1459dd050f965497aeb0c2c99bed3d6af9c
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-06-13 21:20:48 -0400

    Changed datatype.

--- Section7/icecream/IceCreamDemo.java
+++ Section7/icecream/IceCreamDemo.java
@@ -19,7 +19,7 @@ public class IceCreamDemo {
         //Creating an icecream object.
         IceCream Sherbet = new IceCream();
         //Creating a flag.
-        String userContinue = "";
+        int userContinue = "";
         //While the user enters yes.
         while(userContinue.length() > 1)
         {
