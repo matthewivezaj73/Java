eed774fd5bce054c650d10b5bb1b9ed63bd6b42b
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-06-13 21:33:42 -0400

    Unindented.

--- Section7/icecream/IceCreamDemo.java
+++ Section7/icecream/IceCreamDemo.java
@@ -18,17 +18,17 @@ public class IceCreamDemo {
         String selectedTopping;
         //Creating an icecream object.
         IceCream Sherbet = new IceCream();
-            //Asking the user what type of topping they would like to add.
-            System.out.println("Please enter the topping you would like to add:\t");
-            //Allowing the user to enter a topping.
-            
-            //Adding a topping to the icecream.
-            String myTopping = Sherbet.addTopping(keyboard.nextLine());
-            //Printing out the topping.
-            System.out.println("The topping you selected is, " + myTopping);
-            //Printing the details of the icecream.
-            Sherbet.printToppings();
-            //Asking the user if they would like to stop adding toppings.
-            System.out.println("Would you like to add any other toppings?\nPlease enter 0 or 1 (0 for yes and 1 for no):\t");
+        //Asking the user what type of topping they would like to add.
+        System.out.println("Please enter the topping you would like to add:\t");
+        //Allowing the user to enter a topping.
+        
+        //Adding a topping to the icecream.
+        String myTopping = Sherbet.addTopping(keyboard.nextLine());
+        //Printing out the topping.
+        System.out.println("The topping you selected is, " + myTopping);
+        //Printing the details of the icecream.
+        Sherbet.printToppings();
+        //Asking the user if they would like to stop adding toppings.
+        System.out.println("Would you like to add any other toppings?\nPlease enter 0 or 1 (0 for yes and 1 for no):\t");
     }
 }
